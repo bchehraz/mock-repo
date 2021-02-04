e4322f759b36c58fd565abdb2b3fc5bea4c8b21b
Babak Chehraz <bchehraz@redcort.com>
2021-02-04 19:30:40 +0000

    add code in files types: .md,.xml,.gradle,.java,.txt,.json,.js,.map,.m,.ts remove code in files types: .xml,.gradle,.java,.txt,.json,.js,.map,.m,.ts

--- content.java
+++ content.java
@@ -5,6 +5,6 @@ public class Chvuoo {
         System.out.println("xanrh");
         System.out.println("hpmig");
         System.out.println("csfxb");
-        System.out.println("dyffn");
+        System.out.println("dxjoh");
     }
 }
