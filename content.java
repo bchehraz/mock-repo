8ddeaa560144a04a042e2c62ee1c2e291f710e06
Babak Chehraz <bchehraz@redcort.com>
2021-02-11 23:32:10 +0000

    add code in files types: .xml,.gradle,.java,.properties,.txt,.js,.map,.html,.css,.json,.pbxproj remove code in files types: .xml,.gradle,.java,.properties,.txt,.js,.map,.html,.json,.pbxproj

--- content.java
+++ content.java
@@ -5,6 +5,6 @@ public class Chvuoo {
         System.out.println("xanrh");
         System.out.println("hpmig");
         System.out.println("csfxb");
-        System.out.println("tbrya");
+        System.out.println("xenee");
     }
 }
