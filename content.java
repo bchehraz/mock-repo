4ca749cca6d7c980217e538b57b0eb4d18d16a01
Babak Chehraz <bchehraz@redcort.com>
2021-02-15 16:22:24 +0000

    add code in files types: .xml,.java,.properties,.txt,.js,.map,.json,.html remove code in files types: .xml,.java,.properties,.txt,.js,.map,.json,.html,.css

--- content.java
+++ content.java
@@ -6,6 +6,6 @@ public class Chvuoo {
         System.out.println("hpmig");
         System.out.println("csfxb");
         System.out.println("fxoes");
-        System.out.println("owesc");
+        System.out.println("bnfxc");
     }
 }
