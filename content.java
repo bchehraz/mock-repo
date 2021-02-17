74f72f3de5d9eefe46f5760128f201b7316405b8
Babak Chehraz <babak.chehraz@gmail.com>
2021-02-17 22:17:51 +0000

    add code in files types: remove code in files types: .properties,.iml,.xml,.rtf,,.gradle,.java,.json,.txt,.js,.map,.svg,.html,.css,.pro,.bin,.bat,.pbxproj,.xcworkspacedata,.xcscheme,.plist,.swift,.storyboard,.entitlements,.lock,.m,.pch,.h,.xcconfig,.modulemap,.markdown,.sh,.podspec,.c

--- content.java
+++ content.java
@@ -5,6 +5,5 @@ public class Chvuoo {
         System.out.println("xanrh");
         System.out.println("hpmig");
         System.out.println("isylu");
-        System.out.println("pmsfq");
     }
 }
