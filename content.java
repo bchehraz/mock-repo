efc0c94a3249ad96c9db38fe80a6f57120718926
Babak Chehraz <babak.chehraz@gmail.com>
2021-08-16 18:34:11 +0000

    add code in files types: .gradle,.xml,.java,.json,,.properties,.txt,.js,.map,.html,.orig,.pbxproj remove code in files types: .gradle,.java,.xml,.json,.properties,.txt,.js,.map,.html,.bin,.pbxproj

--- content.java
+++ content.java
@@ -6,6 +6,6 @@ public class Chvuoo {
         System.out.println("hpmig");
         System.out.println("fexdf");
         System.out.println("dbrqi");
-        System.out.println("pyepi");
+        System.out.println("iegqh");
     }
 }
